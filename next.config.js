0dbb340d49d9f120d04224c855c41d75efdd1944
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-10-31 09:42:37 +0530

    refc: Mage legacy docs base

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs',
+        permanent: true, // Set to false if you want temporary redirect (307) instead of permanent (308)
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig; 
