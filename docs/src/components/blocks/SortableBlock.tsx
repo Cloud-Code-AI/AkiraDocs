7ddd7a4164bd92c0aa0e16bfc094719bd890fcb8
Shreyash Gupta <48386323+shreyashkgupta@users.noreply.github.com>
2024-11-20 01:06:24 +0530

    refc: Folder change

--- docs/src/components/blocks/SortableBlock.tsx
+++ docs/src/components/blocks/SortableBlock.tsx
@@ -12,7 +12,7 @@ import { useRef, useCallback, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { BlockFormatToolbar } from './BlockFormatToolbar'
+import { BlockFormatToolbar } from '../editor/BlockFormatToolbar'
 
 interface SortableBlockProps {
   block: {
