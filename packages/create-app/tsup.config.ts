10f204445e64b9daed0e50ea984d5472194572a4
Saurav Panda <sgp65@cornell.edu>
2024-11-27 22:02:57 -0800

    Updated readme and template trigger

--- packages/create-app/tsup.config.ts
+++ packages/create-app/tsup.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsup';
 import { chmod } from 'fs/promises';
 import { copyDir, updatePackageJsonVersion } from './scripts/copyTemplate';
 import { readFile } from 'fs/promises';
+import path from 'path';
+
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
@@ -16,9 +18,22 @@ export default defineConfig({
     js: '#!/usr/bin/env node',
   },
   async onSuccess() {
-    // Copy template before making the dist executable
-
+    // Copy main template
     await copyDir('../akiradocs', './template');
+
+    // Copy main README and gifs folder
+    const rootDir = path.resolve(__dirname, '../..');
+    const mainReadmePath = path.join(rootDir, 'README.md');
+    const gifsFolder = path.join(rootDir, 'gifs');
+    
+    // Copy main README to template
+    await copyDir(mainReadmePath, path.join('./template', 'README.md'));
+    await copyDir(mainReadmePath, path.join('./template', '.gitignore'));
+    
+    // Copy gifs folder to template
+    await copyDir(gifsFolder, path.join('./template', 'gifs'));
+
+    // Update package.json version
     const templatePackageJson = JSON.parse(
       await readFile('./package.json', 'utf-8')
     );
